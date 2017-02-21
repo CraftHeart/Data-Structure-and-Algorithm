9e2182cc2b4b50b954b61fba16a714319735fbbd
CraftHeart <1247751874@qq.com>
2017-02-21 21:19:02 +0800

    update

--- 数据结构学习/circle_doubly_linked_list.h
+++ 数据结构学习/circle_doubly_linked_list.h
@@ -23,7 +23,7 @@ public:
 
 	bool is_empty()const { return list_size_ == 0; }
 	T& operator[](const int the_index) const;
-	CircleDoublyLinkedList<T>& operator=(CircleDoublyLinkedList &);
+	CircleDoublyLinkedList<T>& operator=(CircleDoublyLinkedList<T> &);
 	CircleDoublyLinkedList<T>& push_back(const T& the_element);
 	CircleDoublyLinkedList<T>& push_begin(const T& the_element);
 	CircleDoublyLinkedList<T>& insert(const int the_index, const T& the_element);
@@ -34,4 +34,52 @@ private:
 	CircleNode<T>* head_node_;
 };
 
+template<class T>
+CircleDoublyLinkedList<T>::CircleDoublyLinkedList(CircleDoublyLinkedList<T>& the_list)
+{
+
+}
+
+template<class T>
+CircleDoublyLinkedList<T>::~CircleDoublyLinkedList()
+{
+	CircleNode<T>* current_node = head_node_;
+
+}
+
+template<class T>
+T& CircleDoublyLinkedList<T>::operator[](const int the_index) const
+{
+
+}
+
+template<class T>
+CircleDoublyLinkedList<T>& CircleDoublyLinkedList<T>::operator=(CircleDoublyLinkedList<T>& the_list)
+{
+
+}
+
+template<class T>
+CircleDoublyLinkedList<T>& CircleDoublyLinkedList<T>::push_back(const T& the_element)
+{
+
+}
+
+template<class T>
+CircleDoublyLinkedList<T>& CircleDoublyLinkedList<T>::push_begin(const T& the_element)
+{
+
+}
+
+template<class T>
+CircleDoublyLinkedList<T>& CircleDoublyLinkedList<T>::insert(const int the_index, const T& the_element)
+{
+
+}
+
+template<class T>
+CircleDoublyLinkedList<T>& CircleDoublyLinkedList<T>::erase(const int the_index)
+{
+
+}
 #endif // !CIRCLE_DOUBLY_LINKED_LIST_H_
